648b11263cca09441c3913e78e2161324ce9c766
LachlanDow <lachlan.dow@hotmail.co.uk>
2018-03-14 07:54:26 +0000

    Set out outline for the employee class

--- MeetingManager/src/Company.java
+++ MeetingManager/src/Company.java
@@ -2,4 +2,24 @@ import java.util.TreeMap;
 
 public class Company {
 	private TreeMap<String,Employee> employees = new TreeMap<String,Employee>();
+	
+	/**
+	 * se search  method will accept a string of employee id's and find the times where non of the members of staff are in meetings
+	 * @param ids as the array of teh employees ids that the algorithm will run for
+	 * @param startTime as the the time that the algorithm will start from
+	 * @param endTime as the time that the algorithm will stop to search for free times
+	 */
+	public void search(String[] ids, Date startTime, Date endTime) {
+		
+	}
+	
+	public void addEmployee(String id,String Name, String jobTitle) {
+		
+	}
+	public void deleteEmployee(String id) {
+		
+	}
+	public void selectEmployee(String id) {
+		
+	}
 }
