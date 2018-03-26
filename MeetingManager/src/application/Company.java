35250629e6442191e6593e5d6fd6d9c18bd5a8ab
dsewerynski <dsewerynski@dundee.ac.uk>
2018-03-26 18:42:34 +0100

    Removed random javadoc

--- MeetingManager/src/application/Company.java
+++ MeetingManager/src/application/Company.java
@@ -12,14 +12,7 @@ public class Company {
 	 *A binary tree of all the employees in the company
 	 */
 	private static TreeMap<Integer,Employee> employees = new TreeMap<Integer,Employee>();
-	
-	
-	/**
-	 * this search  method will accept a string of employee id's and find the times where non of the members of staff are in meetings
-	 * @param ids as the array of the employees ids that the algorithm will run for
-	 * @param startTime as the the time that the algorithm will start from
-	 * @param endTime as the time that the algorithm will stop to search for free times
-	 */
+
 	
 	/**
 	 * Method to preform a search of available time for the user to pick when to add meetings to
